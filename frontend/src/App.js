886c87c71ec285327fd4b754fbc9830c9e0a9535
Edswel <franckini@yahoo.com>
2022-03-08 09:14:59 +0100

    Add react bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,34 @@
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
 
 
 function App() {
   return (
     <BrowserRouter>
-      <div>
+      <div className='d-flex flex-column site-container'>
         <header>
-          <Link to='/'>PayLess</Link>
+          <Navbar className='navbar-background' variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>PayLess</Navbar.Brand></LinkContainer>
+            </Container>
+          </Navbar>
         </header>
         <main>
-          <Routes>
-            <Route path="/product/:slug" element={<ProductScreen />} />
-            <Route path="/" element={<HomeScreen />} />
-          </Routes>
+          <Container>
+            <Routes>
+              <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/" element={<HomeScreen />} />
+            </Routes>
+          </Container>
         </main>
+        <footer className='footer-background'>
+          <div className='text-center'>© {new Date().getFullYear()} PayLess. All rights reserved.</div>
+        </footer>
       </div>
     </BrowserRouter>
   );
