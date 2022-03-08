152b63b6e4b35416a56b65080c2300aa7d2996d6
Edswel <franckini@yahoo.com>
2022-03-08 07:31:36 +0100

    Create backend api to return list of products

---
+++ backend/data.js
@@ -0,0 +1,66 @@
+const data = {
+    products: [
+        {
+            name: "Air Jordan",
+            slug: "jordan-sneakers",
+            category: "sneakers",
+            image: "/images/pic1.jpg", // 679 x 829
+            price: 250,
+            countInStock: 12,
+            brand: "Nike",
+            rating: 5,
+            numReviews: 7,
+            description: "High Quality Sneakers"
+        },
+        {
+            name: "Red Bottoms",
+            slug: "heels-shoes",
+            category: "shoes",
+            image: "/images/pic2.jpg",
+            price: 150,
+            countInStock: 20,
+            brand: "Louboutin",
+            rating: 4.5,
+            numReviews: 5,
+            description: "Good Quality Shoes"
+        },
+        {
+            name: "Men's Business Suit",
+            slug: "suits",
+            category: "suits",
+            image: "/images/pic3.jpg",
+            price: 350,
+            countInStock: 7,
+            brand: "Gucci",
+            rating: 5,
+            numReviews: 3,
+            description: "High Quality Suit"
+        },
+        {
+            name: "Women's Dress",
+            slug: "fitted-dress",
+            category: "dress",
+            image: "/images/pic4.jpg",
+            price: 100,
+            countInStock: 18,
+            brand: "Chanel",
+            rating: 4,
+            numReviews: 9,
+            description: "Nice Fitted Dress"
+        },
+        {
+            name: "Cap",
+            slug: "fancy-cap",
+            category: "cap",
+            image: "/images/pic5.jpg",
+            price: 50,
+            countInStock: 25,
+            brand: "D & G",
+            rating: 4.5,
+            numReviews: 3,
+            description: "Premium Cap"
+        }
+    ]
+}
+
+export default data;
