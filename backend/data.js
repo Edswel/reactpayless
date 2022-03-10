84ad0f1195d162e7022a2412f6ec035408b5902f
Edswel <franckini@yahoo.com>
2022-03-10 18:43:08 +0100

    Create sample users

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,20 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
+    users: [
+        {
+            name: "Edward",
+            email: "admin@example.com",
+            password: bcrypt.hashSync("12345"),
+            isAdmin: true
+        },
+        {
+            name: "John",
+            email: "user@example.com",
+            password: bcrypt.hashSync("1234567"),
+            isAdmin: false
+        }
+    ],
     products: [
         {
             // _id: "1",
