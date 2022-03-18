3d52d3b0231a0c25dd563a8fd8fb914204be05b9
Edswel <franckini@yahoo.com>
2022-03-18 11:11:33 +0100

    Create search screen

--- frontend/src/screens/SearchScreen.js
+++ frontend/src/screens/SearchScreen.js
@@ -6,8 +6,8 @@ import { toast } from 'react-toastify';
 import { getError } from '../utils';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-// import { LinkContainer } from 'react-router-bootstrap';
-import LinkContainer from 'react-router-bootstrap/LinkContainer';
+import { LinkContainer } from 'react-router-bootstrap';
+// import LinkContainer from 'react-router-bootstrap/LinkContainer';
 import Button from 'react-bootstrap/Button';
 import Rating from '../components/Rating';
 import LoadingBox from '../components/LoadingBox';
@@ -17,6 +17,8 @@ import Product from '../components/Product';
 const reducer = (state, action) => {
     switch (action.type) {
         case "FETCH_REQUEST":
+            return { ...state, loading: true };
+        case "FETCH_SUCCESS":
             return {
                 ...state,
                 products: action.payload.products,
@@ -35,34 +37,34 @@ const reducer = (state, action) => {
 const prices = [
     {
         name: "$1 to $50",
-        value: "1 to 50"
+        value: "1-50"
     },
     {
         name: "$51 to $200",
-        value: "51 to 200"
+        value: "51-200"
     },
     {
         name: "$201 to $1000",
-        value: "201 to 1000"
+        value: "201-1000"
     }
 ];
 
 export const ratings = [
     {
-        name: "4 stars and up",
-        value: 4
+        name: "4stars & up",
+        rating: 4
     },
     {
-        name: "3 stars and up",
-        value: 3
+        name: "3stars & up",
+        rating: 3
     },
     {
-        name: "2 stars and up",
-        value: 2
+        name: "2stars & up",
+        rating: 2
     },
     {
-        name: "1 star and up",
-        value: 1
+        name: "1star & up",
+        rating: 1
     },
 ];
 
@@ -117,7 +119,7 @@ function SearchScreen() {
         const filterRating = filter.rating || rating;
         const filterPrice = filter.price || price;
         const sortOrder = filter.order || order;
-        return `/search?page=${filterPage}&query=${filterQuery}&category=${filterCategory}&price=${filterPrice}&rating=${filterRating}&order=${sortOrder}`;
+        return `/search?category=${filterCategory}&query=${filterQuery}&price=${filterPrice}&rating=${filterRating}&order=${sortOrder}&page=${filterPage}`;
     }
 
     return (
@@ -131,11 +133,21 @@ function SearchScreen() {
                     <div>
                         <ul>
                             <li>
-                                <Link className={"all" === category ? "text-bold" : ""} to={getFilterUrl({ category: "all" })}>Any</Link>
+                                <Link
+                                    className={'all' === category ? 'text-bold' : ''}
+                                    to={getFilterUrl({ category: 'all' })}
+                                >
+                                    Any
+                                </Link>
                             </li>
                             {categories.map((c) => (
                                 <li key={c}>
-                                    <Link className={c === category ? "text-bold" : ""} to={getFilterUrl({ category: c })}>{c}</Link>
+                                    <Link
+                                        className={c === category ? 'text-bold' : ''}
+                                        to={getFilterUrl({ category: c })}
+                                    >
+                                        {c}
+                                    </Link>
                                 </li>
                             ))}
                         </ul>
@@ -144,28 +156,44 @@ function SearchScreen() {
                         <h3>Price</h3>
                         <ul>
                             <li>
-                                <Link className={"all" === price ? "text-bold" : ""} to={getFilterUrl({ price: "all" })}>Any</Link>
+                                <Link
+                                    className={'all' === price ? 'text-bold' : ''}
+                                    to={getFilterUrl({ price: 'all' })}
+                                >
+                                    Any
+                                </Link>
                             </li>
                             {prices.map((p) => (
                                 <li key={p.value}>
-                                    <Link className={p.value === price ? "text-bold" : ""} to={getFilterUrl({ price: p.value })}>{p.name}</Link>
+                                    <Link
+                                        to={getFilterUrl({ price: p.value })}
+                                        className={p.value === price ? 'text-bold' : ''}
+                                    >
+                                        {p.name}
+                                    </Link>
                                 </li>
                             ))}
                         </ul>
                     </div>
                     <div>
-                        <h3>Average Customer Review</h3>
+                        <h3>Avg. Customer Review</h3>
                         <ul>
                             {ratings.map((r) => (
                                 <li key={r.name}>
-                                    <link className={`${r.rating} === ${rating}` ? "text-bold" : ""} to={getFilterUrl({ rating: r.rating })}>
+                                    <Link
+                                        to={getFilterUrl({ rating: r.rating })}
+                                        className={`${r.rating}` === `${rating}` ? 'text-bold' : ''}
+                                    >
                                         <Rating caption={' & up'} rating={r.rating}></Rating>
-                                    </link>
+                                    </Link>
                                 </li>
                             ))}
                             <li>
-                                <Link to={getFilterUrl({ rating: "all" })} className={rating === "all" ? "text-bold" : ""}>
-                                    <Rating caption={" & up"} rating={0}></Rating>
+                                <Link
+                                    to={getFilterUrl({ rating: 'all' })}
+                                    className={rating === 'all' ? 'text-bold' : ''}
+                                >
+                                    <Rating caption={' & up'} rating={0}></Rating>
                                 </Link>
                             </li>
                         </ul>
@@ -178,29 +206,35 @@ function SearchScreen() {
                         <MessageBox variant="danger">{error}</MessageBox>
                     ) : (
                         <>
-                            <Row className='justify-content-between mb-3'>
+                            <Row className="justify-content-between mb-3">
                                 <Col md={6}>
                                     <div>
-                                        {countProducts === 0 ? "No" : countProducts} Results
-                                        {query !== "all" && " : " + query}
-                                        {category !== "all" && " : " + category}
-                                        {price !== "all" && " : Price " + price}
-                                        {rating !== "all" && " : Rating " + rating + " & up"}
-                                        {query !== "all" ||
-                                            category !== "all" ||
-                                            rating !== "all" ||
-                                            price !== "all" ? (
-                                            <Button variant='light' onClick={() => navigate("/")}>
-                                                <i className='fas fa-times-circle'></i>
+                                        {countProducts === 0 ? 'No' : countProducts} Results
+                                        {query !== 'all' && ' : ' + query}
+                                        {category !== 'all' && ' : ' + category}
+                                        {price !== 'all' && ' : Price ' + price}
+                                        {rating !== 'all' && ' : Rating ' + rating + ' & up'}
+                                        {query !== 'all' ||
+                                            category !== 'all' ||
+                                            rating !== 'all' ||
+                                            price !== 'all' ? (
+                                            <Button
+                                                variant="light"
+                                                onClick={() => navigate('/search')}
+                                            >
+                                                <i className="fas fa-times-circle"></i>
                                             </Button>
                                         ) : null}
                                     </div>
                                 </Col>
-                                <Col className='text-end'>
-                                    Sort by{" "}
-                                    <select value={order} onChange={(e) => {
-                                        navigate(getFilterUrl({ order: e.target.value }));
-                                    }}>
+                                <Col className="text-end">
+                                    Sort by{' '}
+                                    <select
+                                        value={order}
+                                        onChange={(e) => {
+                                            navigate(getFilterUrl({ order: e.target.value }));
+                                        }}
+                                    >
                                         <option value="newest">Newest Arrivals</option>
                                         <option value="lowest">Price: Low to High</option>
                                         <option value="highest">Price: High to Low</option>
@@ -209,8 +243,9 @@ function SearchScreen() {
                                 </Col>
                             </Row>
                             {products.length === 0 && (
-                                <MessageBox>No Product Found!</MessageBox>
+                                <MessageBox>No Product Found</MessageBox>
                             )}
+
                             <Row>
                                 {products.map((product) => (
                                     <Col sm={6} lg={4} className="mb-3" key={product._id}>
@@ -218,15 +253,20 @@ function SearchScreen() {
                                     </Col>
                                 ))}
                             </Row>
+
                             <div>
                                 {[...Array(pages).keys()].map((x) => (
                                     <LinkContainer
                                         key={x + 1}
                                         className="mx-1"
-                                        to={getFilterUrl({ page: x + 1 })}>
+                                        to={getFilterUrl({ page: x + 1 })}
+                                    >
                                         <Button
-                                            className={Number(page) === x + 1 ? "text-bold" : ""}
-                                            variant="light">{x + 1}</Button>
+                                            className={Number(page) === x + 1 ? 'text-bold' : ''}
+                                            variant="light"
+                                        >
+                                            {x + 1}
+                                        </Button>
                                     </LinkContainer>
                                 ))}
                             </div>
